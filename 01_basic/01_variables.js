dff93ea7bfdb593bc120bb2ab238544aec4e3463
sudhansukhandai <sudhansukhandai@gmail.com>
2024-04-19 09:53:57 +0000

    datatype done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 123456
+let accountEmail = "sudhansukhandai@gmail.com"
+var accountPassword = "Sekhar@123"
+accountCity = "Bangalore"
+
+//accountId = 2
+accountEmail = "s@gmail.com"
+accountPassword = "Se@123"
+accountCity = "Bhubaneswar"
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity]);
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,21 @@
+"use strict" // treat all code in newer version.
+alert(3+3) //we are using node js not browser
+
+let name = "Sudhansu"
+let age = 28
+let isLoggedIn = false
+
+// number => 2 to power of  52
+// bigint
+// string => ""
+// boolen => true/false
+// null => standed alone value (empty value)
+// undefined => variable is declared but value is not assigned.
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+

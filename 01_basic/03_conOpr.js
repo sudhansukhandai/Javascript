c845d470f55d4da0369e2f45bb646d314e29a44d
sudhansukhandai <sudhansukhandai@gmail.com>
2024-04-20 08:12:35 +0000

    done

--- 01_basic/03_conOpr.js
+++ 01_basic/03_conOpr.js
@@ -1,3 +1,25 @@
- let score = "33"
-console.log(typeof score);
- 
+ /* let score = "33abc"
+ console.log(typeof score);
+ console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let static = null
+let staticInNumber = Number(static)
+console.log(typeof staticInNumber)
+console.log(staticInNumber)
+
+let staticScore = false
+let staticInScore = Number(staticScore)
+console.log(typeof staticInScore)
+console.log(staticInScore) */
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+let isloggedIn = 1
+let boolenIsLoggedin = Boolean(isloggedIn)
+console.log(boolenIsLoggedin)
